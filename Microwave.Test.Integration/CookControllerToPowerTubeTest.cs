4c5e53a61ffcbaa53ef18e6394bf18b23fdfb057
Oleksandr Zubrytskyy <201908679@post.au.dk>
2021-04-29 22:12:54 +0200

    CookControllerToPowerTubeTest tilføjet

---
+++ Microwave.Test.Integration/CookControllerToPowerTubeTest.cs
@@ -0,0 +1,73 @@
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Microwave.Test.Integration
+{
+    class CookControllerToPowerTubeTest
+    {
+        private Output output_;
+        private PowerTube PT_;
+        private Display display_;
+        private CookController CC_;
+        private System.IO.StringWriter SW_;
+        private ITimer timer_;
+        private IUserInterface UI_;
+     
+        [SetUp]
+        public void Setup()
+        {
+            output_ = new Output();
+            PT_ = new PowerTube(output_);
+            display_ = new Display(output_);
+            timer_ = Substitute.For<ITimer>();
+            UI_ = Substitute.For<IUserInterface>();
+            CC_ = new CookController(timer_, display_, PT_, UI_);
+        }
+
+        [TestCase(50)]
+        [TestCase(250)]
+        [TestCase(450)]
+        [TestCase(700)]
+        public void StartOwnsetTurnOn(int power)
+        {
+            SW_ = new StringWriter();
+            Console.SetOut(SW_);
+            int time = 60;
+            CC_.StartCooking(power, time);
+            string str = ($"PowerTube works with {power}");
+            StringAssert.Contains(str, SW_.ToString());
+        }
+        [Test]
+        public void CalledTurnOff()
+        {
+            int time = 60;
+            int power = 200;
+            string str = "PowerTube turned off";
+            CC_.StartCooking(power, time);
+            SW_ = new StringWriter();
+            Console.SetOut(SW_);
+            CC_.Stop();
+            StringAssert.Contains(str, SW_.ToString());
+        }
+
+        [Test]
+        public void CalledTurnOffTimerExpired()
+        {
+            int time = 60;
+            int power = 100;
+            string str = "PowerTube turned off";
+            CC_.StartCooking(power, time);
+            SW_ = new StringWriter();
+            Console.SetOut(SW_);
+            timer_.Expired += Raise.Event();
+            StringAssert.Contains(str, SW_.ToString());
+        }
+
+
+    }
+}
